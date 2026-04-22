950b0c9515536df2c1c8bcc0e3022d6db0c558fa
bapheleledlamini <bapheleledlamini211@gmail.com>
2026-04-22 16:57:24 +0200

    Add Beverage class extending Item

--- src/ub/com/csi142/tuckshop/products/Beverage.java
+++ src/ub/com/csi142/tuckshop/products/Beverage.java
@@ -1,1 +1,7 @@
+package ub.com.csi142.tuckshop.products;
 
+public class Beverage extends Item{
+  public Beverage(String name, double price, int quantity, int id){
+    super(name, price, quantity, id)
+  }
+}
