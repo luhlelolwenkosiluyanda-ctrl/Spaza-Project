5391dbdd6823165ddfe914634ac2a7c8b776f921
phetsa5dlamini-beep <phetsa5dlamini@gmail.com>
2026-04-26 18:57:49 +0200

    Update Snack.java
    Initialization  of the Snack.java class which extends the Item.java class. It also overrides the describeItem method

--- src/ub/com/csi142/tuckshop/products/nack.java
+++ src/ub/com/csi142/tuckshop/products/nack.java
@@ -1,1 +1,16 @@
-public class 
+public class Snack extends Item{
+    private String id;
+
+    public  Snack(String name, double price, int quantity, String id){
+        super(name, price, quantity);
+        this.id = id;
+    }
+    public String getID(){
+        return id;
+    }
+    
+    @Override
+    public String describeItem(){
+        return "Snack details: "+ getName() +"|| " + getPrice() + " || " + getQuantity() + "units || ID: "+ getID();
+    }
+}
