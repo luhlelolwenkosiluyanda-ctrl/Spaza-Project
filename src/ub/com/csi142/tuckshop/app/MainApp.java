3f1794240b1c862d370ca896e38fcec01504740a
Wandzile Khumalo <khumalomwandzi@gmail.com>
2026-04-30 21:38:19 +0200

    Update MainApp.java

--- src/ub/com/csi142/tuckshop/app/MainApp.java
+++ src/ub/com/csi142/tuckshop/app/MainApp.java
@@ -1,9 +1,9 @@
 
 package com.ub.csi142.tuckshop;
-
+import ub.com.csi142.tuckshop.utils.InputValidator;
 import java.util.Scanner;
 
-public class TuckshopApp {
+public class MainApp {
 
     public static void main(String[] args) {
 
@@ -11,28 +11,44 @@ public class TuckshopApp {
         boolean running = true;
 
         while (running) {
-            System.out.println("--- Tuckshop Management System ---");
+            System.out.println("\n--- Tuckshop Management System ---");
             System.out.println("1. View Inventory");
             System.out.println("2. Add New Item");
             System.out.println("3. Record a Sale");
             System.out.println("4. View Sales Summary");
             System.out.println("5. Exit");
             System.out.print("Select an option: ");
-
-            int choice = input.nextInt();
-
+            
+            int choice = InputValidator.getValidInt("Select an option: ");
             switch (choice) {
                 case 1:
-                    // Call inventory.displayDetails()
+                    // Call inventory.displayDetails().
+                    System.out.println("Displaying Inventory...");
+                    System.out.println("\n--Current Inventory--");
+                    Inventory.displayItems();
+                    
                     break;
                 case 2:
-                    // Logic to create a new Item object
+                    System.out.println("\n---Add New Item---");
+                    String name = InputValidator.getNoneEmptyString("Enter item name: ");
+                    double price = InputValidator.getValidPrice("Enter item price: P");
+                    int qty = InputValidator.getValidInt("Enter quantity: ");
+                    //Logic to create a new item.
+                    Inventory.addItem();
+                    System.out.println("--Successfully added item into the system!");
                     break;
                 case 3:
-                    // Logic to find product and reduce quantity
+                    /*
+                    System.out.println("Enter item name: ");
+                    String saleItem = input.next();
+                    System.out.println("Enter quantity: ");
+                    int saleQty = InputValidator.getValidInt("");
+                    */
+
+                    processSale();
                     break;
                 case 4:
-                    // Logic to iterate through saleSummary
+                    printAllSales();
                     break;
                 case 5:
                     running = false;
