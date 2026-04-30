74017b1e55d91c0e7f90f24b84ac67544b3594b1
luhlelolwenkosiluyanda-ctrl <luhlelolwenkosiluyanda@gmail.com>
2026-04-30 21:03:29 +0200

    Refactor Inventory class to use Item type
    Updated the Inventory class to use Item instead of Product and fixed the for loop syntax.

--- src/ub/com/csi142/tuckshop/services/Inventory.java
+++ src/ub/com/csi142/tuckshop/services/Inventory.java
@@ -1,20 +1,20 @@
 package ub.com.csi142.tuckshop.services;
-
+import ub.com.csi142.tuckshop.products;
 import java.util.ArrayList;
 
 public Class Inventory {
 
         private ArrayList<Item> items = new ArrayList<>();
     
-    public void addProduct(Product product) {
-        items.add(product);
+    public void addProduct(Item item) {
+        items.add(item);
     }
     public void displayDetails() {
         if (items.isEmpty()) {
             System.out.println("Inventory is empty.");
         } else {
             System.out.println("---Current Invetory---");
-            for (Item i | products) {
+            for (Item i | items) {
                 System.out.println(i);
             }
         }
