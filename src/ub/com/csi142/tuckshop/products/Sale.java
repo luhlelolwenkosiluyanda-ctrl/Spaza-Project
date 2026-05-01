21d4ae9312cfd4970c007e9302779e0fd578d621
phetsa5dlamini-beep <phetsa5dlamini@gmail.com>
2026-05-01 19:48:25 +0200

    Update Sale.java

--- src/ub/com/csi142/tuckshop/products/Sale.java
+++ src/ub/com/csi142/tuckshop/products/Sale.java
@@ -1,1 +1,28 @@
 
+package ub.com.csi142.tuckshop.productsl;
+
+public class Sale {
+    private  Item item;
+    private int quantity;
+
+    public Sale(Item item, int quantity){
+        this.item =item;
+        this.quantity =quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public double getTotalPrice(){
+        return item.getPrice() * quantity; 
+    }
+
+    public void displaySale() {
+        // TODO Auto-generated method stub
+        System.out.println("Sale details: " + getItem().getName() + " || Quantity: " + getQuantity() + " || Total Price: " + getTotalPrice());
+        // throw new UnsupportedOperationException("Unimplemented method 'displaySale'");
+    }
+}
