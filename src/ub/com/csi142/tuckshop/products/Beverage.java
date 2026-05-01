a38a0ff4741766e19de76317f365bde4f27d086a
luhlelolwenkosiluyanda-ctrl <luhlelolwenkosiluyanda@gmail.com>
2026-05-01 23:01:21 +0200

    Remove redundant Beverage class definition

--- src/ub/com/csi142/tuckshop/products/Beverage.java
+++ src/ub/com/csi142/tuckshop/products/Beverage.java
@@ -1,4 +1,3 @@
- dev-Nsizwa
 package ub.com.csi142.tuckshop.products;
 
 public class Beverage extends Item{
@@ -19,25 +18,4 @@ public class Beverage extends Item{
   
     }
 }
-
-package ub.com.csi142.tuckshop.products;
-
-public class Beverage extends Item{
-  private String id;
-  
-  public Beverage(String name, double price, int stockLevel, String id){
-    super(name, price, stockLevel);
-    this.id = id;
-  }
-  
-    public String getID(){
-      return id;
-    }
-  
-  @Override 
-  public String toString(){
-    return "Drink details: " + getName() + " || P" + getPrice() + " || " + getStockLevel() + " units || ID: " +  getID();
-  
-    }
-}
- main
+ 
