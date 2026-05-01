a62fcafafadb2075d7c6804d31904b30adc89085
luhlelolwenkosiluyanda-ctrl <luhlelolwenkosiluyanda@gmail.com>
2026-05-01 23:03:36 +0200

    Clean up snack.java by removing duplicates
    Removed duplicate package declaration and unnecessary code.

--- src/ub/com/csi142/tuckshop/products/snack.java
+++ src/ub/com/csi142/tuckshop/products/snack.java
@@ -1,5 +1,4 @@
- dev-Nsizwa
-package ub.com.csi142.tuckshop.products;
+ package ub.com.csi142.tuckshop.products;
 
 public class Snack extends Item{
     private String id;
@@ -18,23 +17,4 @@ public class Snack extends Item{
     }
 }
 
-package ub.com.csi142.tuckshop.products;
-
-public class Snack extends Item{
-    private String id;
-
-    public  Snack(String name, double price, int quantity, String id){
-        super(name, price, quantity);
-        this.id = id;
-    }
-    public String getID(){
-        return id;
-    }
-    
-    @Override
-    public String toString(){
-        return "Snack details: "+ getName() +"|| " + getPrice() + " || " + getStockLevel() + "units || ID: "+ getID();
-    }
-}
-
- main
+ 
