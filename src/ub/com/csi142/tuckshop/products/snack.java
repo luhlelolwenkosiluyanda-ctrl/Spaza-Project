2e7bb866a7026d8b51334d9d19277675481b5142
Lebogang-Gwetshwayo <gwetshwayolebogang@gmail.com>
2026-05-01 22:39:56 +0200

    Create snack.java

---
+++ src/ub/com/csi142/tuckshop/products/snack.java
@@ -0,0 +1,18 @@
+package ub.com.csi142.tuckshop.products;
+
+public class Snack extends Item{
+    private String id;
+
+    public  Snack(String name, double price, int quantity, String id){
+        super(name, price, quantity);
+        this.id = id;
+    }
+    public String getID(){
+        return id;
+    }
+    
+    @Override
+    public String toString(){
+        return "Snack details: "+ getName() +"|| " + getPrice() + " || " + getStockLevel() + "units || ID: "+ getID();
+    }
+}
